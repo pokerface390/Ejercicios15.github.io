5aeaca805e5bef43d850f5cb4e6e7974d710db17
JuanCarlosHdzVideos <metin2chiri@gmail.com>
2025-07-14 19:32:54 -0600

    Deploy Angular build to GitHub Pages

---
+++ docs/server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'C:/Program Files/Git/EjerciciosRepo.github.io',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
